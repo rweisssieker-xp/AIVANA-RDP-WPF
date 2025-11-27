06064316d7a17f67ac8c3bf24687ff0fb7e42cc6
rweisssieker-xp <rweisssieker@gmail.com>
2025-11-27 10:54:09 +0100

    3

---
+++ Aivana_RDP_WPF/Converters/InverseBooleanConverter.cs
@@ -0,0 +1,29 @@
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Aivana_RDP_WPF.Converters;
+
+/// <summary>
+/// Inverts a boolean value.
+/// </summary>
+public class InverseBooleanConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is bool boolValue)
+        {
+            return !boolValue;
+        }
+        return true;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is bool boolValue)
+        {
+            return !boolValue;
+        }
+        return false;
+    }
+}
+
